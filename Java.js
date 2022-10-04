3ac5dd00eaac3fb9ce438e2960d78e543bce123b
Veronique2207 <bulongoveronique@gmail.com>
2022-10-04 15:35:04 +0200

    adding animation to heading

---
+++ Java.js
@@ -0,0 +1,12 @@
+const app=Vue.createApp
+    Data()
+      return{
+        showMovie: true,
+           movies:[
+            {title:'kung fu panda', duration 2,img:'.images/kung fu panda.jpg,'isfav:true},
+            {title:'little mermaid', duration 1,img:'.images/little mermaid,'isfav:true},
+            {title:'wakanda ', duration 2,img:'.images/wakanda.jpeg,'isfav:true},
+            {title:'cinderella', duration 2,img:'.images/cinderella.jpg,'isfav:false},
+           ]
+
+      }
